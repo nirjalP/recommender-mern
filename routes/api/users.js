146412276de2b8fc4c49c804374693032120e781
flyingDutchman01 <nirjalprajapati@gmail.com>
2020-11-24 22:23:58 +0545

    errors1

--- routes/api/users.js
+++ routes/api/users.js
@@ -17,7 +17,7 @@ router.post('/',[
 async (req,res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(400).json({errors:error.array()});
+        return res.status(400).json({errors:errors.array()});
     }
     
     const { name,email,password } = req.body;
